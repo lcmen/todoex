60b15724529cd3b089d82e58372a7a2df1799a72
Lucas Mendelowski <lucas@mendelowski.com>
2021-09-27 22:44:20 +0200

    Convert todo list to process based approach

--- todo_list.ex
+++ todo_list.ex
@@ -36,25 +36,59 @@ defmodule TodoList do
   end
 end
 
-defmodule TodoList.CsvImporter do
-  def import(file_path) do
-    file_path
-    |> read_file()
-    |> parse()
-    |> TodoList.new()
+defmodule TodoServer do
+  def start do
+    spawn(fn ->
+      Process.register(self(), :todo_server)
+      loop(TodoList.new())
+    end)
   end
 
-  defp read_file(file_path) do
-    file_path
-    |> File.stream!()
-    |> Stream.map(&String.replace(&1, "\n", ""))
+  def add_entry(new_entry) do
+    send(:todo_server, {:add_entry, new_entry})
   end
 
-  defp parse(lines) do
-    lines
-    |> Stream.map(&String.split(&1, ","))
-    |> Stream.map(fn [date, title] -> %{date: date, title: title} end)
+  def delete_entry(entry_id) do
+    send(:todo_server, {:delete_entry, entry_id})
+  end
+
+  def update_entry(entry) do
+    send(:todo_server, {:update_entry, entry})
+  end
+
+  def entries(date) do
+    send(:todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  defp process(todo_list, {:add_entry, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process(todo_list, {:delete_entry, entry_id}) do
+    TodoList.delete_entry(todo_list, entry_id)
+  end
+
+  defp process(todo_list, {:update_entry, entry}) do
+    TodoList.update_entry(todo_list, entry)
+  end
+
+  defp process(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
   end
 end
-
-IO.inspect TodoList.CsvImporter.import("./todos.csv")
